f47ce70f07dcf42f22317310a4651b263b351657
luisholman <88738669+luisholman@users.noreply.github.com>
2023-07-08 11:03:10 -0500

    commit

--- src/Componentes/Paginas/Skills/index.jsx
+++ src/Componentes/Paginas/Skills/index.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import './Service.css';
+import './Skill.css';
+import imgHtml from '../img/html5.png';
+import imgCss from '../img/css3.png';
+import imgBootstrap from '../img/bootstrap.png';
+import imgJs from '../img/js.png';
+import imgGitHub from '../img/imggithub.png';
+import imgMysql from '../img/mysql.png';
 
 function Skills ()  {
     return (
@@ -21,32 +27,16 @@ function Skills ()  {
                     <div className="col__3">
                          <div className="service__box pointer">
                             <div className="icon">
-                            <svg
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M2 16.1A5 5 0 0 1 5.9 20M2 12.05A9 9 0 0 1 9.95 20M2 8V6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-6"></path>
-                  <line x1="2" y1="20" x2="2" y2="20"></line>
-                </svg>
+
+                                <img className="img" src={imgHtml} alt="" />
+                           
                             </div>
                             <div className="service__meta">
-                                <h1 className="service__text">Business Stratagy</h1>
-                                <p className="p service__text p__color">
+                                <h1 className="service__text">HTML</h1>
+                                {/*<p className="p service__text p__color">
                                     I throw myself down among the
                                 </p>
-                                <p className="p service__text p__color">
-                                   tall grass by the stream as I lie
-                                </p>
-                                <p className="p service__text p__color">
-                                    Close to the earth.
-                                </p>
+                                >*/}
                             </div>
                          </div>
                     </div>
@@ -54,33 +44,13 @@ function Skills ()  {
                     <div className="col__3">
                          <div className="service__box pointer">
                             <div className="icon">
-                            <svg
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
-                  <polyline points="2 17 12 22 22 17"></polyline>
-                  <polyline points="2 12 12 17 22 12"></polyline>
-                </svg>
+                            
+                            <img className="img" src={imgCss} alt="" />
+
                             </div>
                             <div className="service__meta">
-                                <h1 className="service__text">Business Stratagy</h1>
-                                <p className="p service__text p__color">
-                                    I throw myself down among the
-                                </p>
-                                <p className="p service__text p__color">
-                                   tall grass by the stream as I lie
-                                </p>
-                                <p className="p service__text p__color">
-                                    Close to the earth.
-                                </p>
+                                <h1 className="service__text">CSS</h1>
+                                
                             </div>
                          </div>
                     </div>
@@ -88,34 +58,13 @@ function Skills ()  {
                     <div className="col__3">
                          <div className="service__box pointer">
                             <div className="icon">
-                            <svg
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                  <circle cx="9" cy="7" r="4"></circle>
-                  <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-                  <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-                </svg>
+                            
+                            <img className="img" src={imgBootstrap} alt="" />
+
                             </div>
                             <div className="service__meta">
-                                <h1 className="service__text">Marketing & Reporting</h1>
-                                <p className="p service__text p__color">
-                                    I throw myself down among the
-                                </p>
-                                <p className="p service__text p__color">
-                                   tall grass by the stream as I lie
-                                </p>
-                                <p className="p service__text p__color">
-                                    Close to the earth.
-                                </p>
+                                <h1 className="service__text">BOOTSTRAP</h1>
+                                
                             </div>
                          </div>
                     </div>
@@ -123,33 +72,13 @@ function Skills ()  {
                     <div className="col__3">
                          <div className="service__box pointer">
                             <div className="icon">
-                            <svg
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
-                  <line x1="8" y1="21" x2="16" y2="21"></line>
-                  <line x1="12" y1="17" x2="12" y2="21"></line>
-                </svg>
+
+                            <img className="img" src={imgJs} alt="" />
+                            
                             </div>
                             <div className="service__meta">
-                                <h1 className="service__text">Mobile App Development</h1>
-                                <p className="p service__text p__color">
-                                    I throw myself down among the
-                                </p>
-                                <p className="p service__text p__color">
-                                   tall grass by the stream as I lie
-                                </p>
-                                <p className="p service__text p__color">
-                                    Close to the earth.
-                                </p>
+                                <h1 className="service__text">JAVASCRIPT</h1>
+                                
                             </div>
                          </div>
                     </div>
@@ -157,34 +86,13 @@ function Skills ()  {
                     <div className="col__3">
                          <div className="service__box pointer">
                             <div className="icon">
-                            <svg
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                  <circle cx="9" cy="7" r="4"></circle>
-                  <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-                  <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-                </svg>
+                            
+                            <img className="img" src={imgGitHub} alt="" />
+
                             </div>
                             <div className="service__meta">
-                                <h1 className="service__text">Business Stratagy</h1>
-                                <p className="p service__text p__color">
-                                    I throw myself down among the
-                                </p>
-                                <p className="p service__text p__color">
-                                   tall grass by the stream as I lie
-                                </p>
-                                <p className="p service__text p__color">
-                                    Close to the earth.
-                                </p>
+                                <h1 className="service__text">GITHUB</h1>
+                                
                             </div>
                          </div>
                     </div>
@@ -192,33 +100,13 @@ function Skills ()  {
                     <div className="col__3">
                          <div className="service__box pointer">
                             <div className="icon">
-                            <svg
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
-                  <line x1="8" y1="21" x2="16" y2="21"></line>
-                  <line x1="12" y1="17" x2="12" y2="21"></line>
-                </svg>
+
+                            <img className="img" src={imgMysql} alt="" />
+                            
                             </div>
                             <div className="service__meta">
-                                <h1 className="service__text">Mobile App Development</h1>
-                                <p className="p service__text p__color">
-                                    I throw myself down among the
-                                </p>
-                                <p className="p service__text p__color">
-                                   tall grass by the stream as I lie
-                                </p>
-                                <p className="p service__text p__color">
-                                    Close to the earth.
-                                </p>
+                                <h1 className="service__text">MYSQL</h1>
+                                
                             </div>
                          </div>
                     </div>
